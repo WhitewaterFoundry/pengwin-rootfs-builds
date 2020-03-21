68d9f9405b58ba86ab16ef53f74bb0b0e1f57d39
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-03-21 19:39:48 +0100

    Implement image generation and upgrade using Vagrant

---
+++ update-targz.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+export PREBOOTSTRAP_ARCH="${1:-amd64}"
+export PREBOOTSTRAP_QEMU_ARCH="${2:-x86_64}"
+export PREBOOTSTRAP_RELEASE="${3:-testing}"
+
+echo 'Installing build dependencies'
+sudo apt-get update -y -q
+sudo apt-get install -y -q curl gnupg debootstrap qemu-user-static
+
+echo 'Creating rootfs folder'
+mkdir rootfs
+
+echo 'Extract previous rootfs, entering chroot to mount dev, sys, proc and dev/pts'
+(
+  # shellcheck disable=SC2164
+  cd rootfs
+  sudo tar -zxvf /vagrant/build/install_"${PREBOOTSTRAP_ARCH}"_rootfs.tar.gz
+  sudo mount --bind /dev dev/
+  sudo mount --bind /sys sys/
+  sudo mount --bind /proc proc/
+  sudo mount --bind /dev/pts dev/pts/
+)
+
+echo 'Copy static QEMU to rootfs'
+sudo cp /usr/bin/qemu-"${PREBOOTSTRAP_QEMU_ARCH}"-static rootfs/usr/bin/
+
+echo "Marking static [rootfs/usr/bin/qemu-${PREBOOTSTRAP_QEMU_ARCH}-static] as executable"
+sudo chmod +x rootfs/usr/bin/qemu-"${PREBOOTSTRAP_QEMU_ARCH}"-static
+
+echo 'Copy dns'
+sudo cp /etc/resolv.conf rootfs/etc/
+
+echo 'Upgrade packages and install more'
+sudo chroot rootfs/ apt-get -y -q update
+sudo chroot rootfs/ apt-get -y -q install man-db socat
+sudo chroot rootfs/ apt-get -y -q upgrade
+
+echo 'Clean up apt cache'
+sudo chroot rootfs/ apt-get -y -q remove systemd dmidecode --allow-remove-essential
+sudo chroot rootfs/ apt-get -y -q autoremove
+sudo chroot rootfs/ apt-get -y -q autoclean
+sudo chroot rootfs/ apt-get -y -q clean
+
+echo 'Add defaults for readline and vim'
+sudo chroot rootfs/ /bin/bash -c "echo 'source /etc/vim/vimrc' > /etc/skel/.vimrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set background=dark' >> /etc/skel/.vimrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set visualbell' >> /etc/skel/.vimrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set noerrorbells' >> /etc/skel/.vimrc"
+sudo chroot rootfs/ /bin/bash -c "echo '\$include /etc/inputrc' > /etc/skel/.inputrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set bell-style none' >> /etc/skel/.inputrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set show-all-if-ambiguous on' >> /etc/skel/.inputrc"
+sudo chroot rootfs/ /bin/bash -c "echo 'set show-all-if-unmodified on' >> /etc/skel/.inputrc"
+
+echo 'Deleting QEMU from chroot'
+sudo rm rootfs/usr/bin/qemu-"${PREBOOTSTRAP_QEMU_ARCH}"-static
+
+echo 'Compressing rootfs'
+rm /vagrant/build/install_"${PREBOOTSTRAP_ARCH}"_rootfs.tar.gz.bak
+mv /vagrant/build/install_"${PREBOOTSTRAP_ARCH}"_rootfs.tar.gz /vagrant/build/install_"${PREBOOTSTRAP_ARCH}"_rootfs.tar.gz.bak
+(
+  # shellcheck disable=SC2164
+  cd rootfs
+  sudo tar -zcvf /vagrant/build/install_"${PREBOOTSTRAP_ARCH}"_rootfs.tar.gz --exclude proc --exclude dev --exclude sys ./*
+)
